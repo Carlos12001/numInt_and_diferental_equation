93b6c2b78b43c8dc0a3df319a1748baf9bc6c38d
Carlos12001 <carlos.andres12001@gmail.com>
2023-06-18 14:22:12 -0600

    feat: simpson method

---
+++ Parte 1/simpson_compuesto.m
@@ -0,0 +1,36 @@
+function I = simpson_compuesto(f,a,b,n)
+  % Esta funcion realiza la aproximación numérica de la función f
+  % dividida en n subintervalos evaluada desde a hasta b, aplicando el 
+  % método de simpson a cada subintervalo
+  % Sintaxis: I = simpson_compuesto(f, a, b, n)
+  % Parametros de entrada:
+  % f = función a evaluar
+  % a y b = rangos de análisis. a sería el inicial y b el final ([a,b])
+  % n = Cantidad de intervalos a dividir de la función f
+  % Parametros de salida
+  % I = Aproximación de f por medio del método de simpson compuesto
+  h = (b-a)/(n-1);
+  soporte = a:h:b;
+
+  I = 0;
+
+  for i=1:n-1
+    I = I + simpson(f, soporte(i), soporte(i+1));
+  end
+
+end
+
+function I = simpson(f, a, b)
+  % Esta funcion realiza la aproximación numérica de la función f por 
+  % medio del método de simpson
+  % Sintaxis: I = simpson(f, a, b)
+  % Parametros de entrada:
+  % f = función a evaluar
+  % a y b = rangos de análisis. a sería el inicial y b el final ([a,b])
+  % Parametros de salida
+  % I = Aproximación de f por medio del método de simpson
+  fn = str2func(["@(x) " f]);
+
+  I = (b - a) / 6 * (fn(a) + 4 * fn((a+b)/2) + fn(b) );
+
+end
---
+++ Parte 1/simpson_compuesto_iterativa.m
@@ -0,0 +1,27 @@
+function I = simpson_compuesto_iterativa(f,a,b,tol=eps,iterMax=2500)
+  % Esta funcion realiza la aproximación numérica de la función f dividida
+  % en i subintervalos evaluada desde a hasta b, aplicando el método de
+  % simpson compuesto a cada subintervalo. Para ello se debe de indicar 
+  % una tolerancia que pueda soportar la cota de error, de manera que se 
+  % utilice una cantidad de subintervalos que minimice al máximo estos 
+  % errores.
+  % Sintaxis: I = simpson_compuesto_iterativa(f,a,b,tol,iterMax)
+  % Parametros de entrada:
+  % f = función a evaluar
+  % a y b = rangos de análisis. a sería el inicial y b el final ([a,b])
+  % tol = tolerancia de error permitida (por defecto es el cero de la máquina)
+  % iterMax = número máximo de iteraciones permitida (por defecto 2500)
+  % Parametros de salida
+  % I = Aproximación de f por medio del método de simpson compuesto iterativo
+
+  I = 0;
+  for i = 2:iterMax
+    Inew = simpson_compuesto(f,a,b,i);
+    err = abs(Inew - I);
+    I = Inew;
+    if abs(err) < tol
+      break;
+    end
+  end
+
+end
